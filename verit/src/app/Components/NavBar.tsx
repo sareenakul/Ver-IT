c43770b522eca996cb66d05ccd5f3b8491bcac14
Akul Sareen <sareen.akul@gmail.com>
2024-10-16 15:26:13 -0400

    Implementing Clerk Auth

--- verit/src/app/Components/NavBar.tsx
+++ verit/src/app/Components/NavBar.tsx
@@ -1,12 +1,20 @@
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import logo from "../Assets/excel.png"
 
 type Props = {}
 
 const NavBar = (props: Props) => {
   return (
     <div className='px-8 py-2 border-b-[1px]'>
-        <div className='flex items-center space-x-3'></div>
-
+        <div className='flex items-center space-x-3'>
+            <Link href="/">
+                <Image src={logo} width={40} height={40} priority alt='Verit-Icon'/>
+            </Link>
+            <UserButton signInUrl='/'/>
+        </div>
     </div>
   )
 }
