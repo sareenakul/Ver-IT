ee7698ea7f775fa74be3f6d2be9b743c8a24bdbc
Akul Sareen <sareen.akul@gmail.com>
2024-10-16 14:24:04 -0400

    Initial set-up

---
+++ verit/src/app/Components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const NavBar = (props: Props) => {
+  return (
+    <div className='px-8 py-2 border-b-[1px]'>
+        <div className='flex items-center space-x-3'></div>
+
+    </div>
+  )
+}
+
+export default NavBar
