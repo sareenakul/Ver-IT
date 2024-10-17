063225198b2a3b3955822bff23d69de80aaeb9b8
Akul Sareen <sareen.akul@gmail.com>
2024-10-17 16:28:57 -0400

    Boilerplate setup fix

---
+++ graphQLServer/server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+
+app.listen(port, console.log(`Server running on: ${port}`))
