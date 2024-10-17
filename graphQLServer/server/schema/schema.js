98e5d644372127cf4c0a426691594ac188f54e74
Akul Sareen <sareen.akul@gmail.com>
2024-10-17 19:04:18 -0400

    Completed the root queries for the application

--- graphQLServer/server/schema/schema.js
+++ graphQLServer/server/schema/schema.js
@@ -1,1 +1,113 @@
-const {users, posts} = require('../sampleData');
+const {users, posts, likes} = require('../sampleData');
+
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList, GraphQLSchema} = require('graphql');
+
+
+// USER TYPE
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: GraphQLID},
+        username: {type: GraphQLString},
+        email: {type: GraphQLString},
+        posts: {type: new GraphQLList(PostType)}
+    })
+})
+
+const PostType = new GraphQLObjectType({
+    name: 'Post',
+    fields: () => ({
+        id: {type: GraphQLID},
+        title: {type: GraphQLString},
+        content: {type: GraphQLString},
+        authorId: {type: GraphQLID},
+        comments: {type: GraphQLList(CommentType)},
+        likes: {type: GraphQLList(LikeType)},
+        tags: {type: GraphQLList(TagType)}
+    })
+});
+
+const CommentType = new GraphQLObjectType({
+    name: 'Comment',
+    fields: () => ({
+        id: { type: GraphQLID },
+        postId: { type: GraphQLID },
+        authorId: { type: GraphQLID },
+        content: { type: GraphQLString },
+        createdAt: { type: GraphQLString }
+    })
+});
+
+const LikeType = new GraphQLObjectType({
+    name: 'Like',
+    fields: () => ({
+        id: { type: GraphQLID },
+        postId: { type: GraphQLID },
+        createdAt: { type: GraphQLString }
+    })
+});
+
+const TagType = new GraphQLObjectType({
+    name: 'Tag',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        posts: { type: new GraphQLList(PostType) } // Reference to PostType
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args){
+                return users;
+            }
+        },
+        posts: {
+            type: new GraphQLList(PostType),
+            resolve(parent, args){
+                return posts;
+            }
+        },
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                return users.find(user => user.id === args.id);
+            }
+        },
+        post: {
+            type: PostType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                return posts.find(post => post.id === args.id); // Return post by ID
+            }
+        },
+        comments: {
+            type: new GraphQLList(CommentType),
+            resolve(parent, args) {
+                return comments; // Return all comments
+            }
+        },
+        likes: {
+            type: new GraphQLList(LikeType),
+            resolve(parent, args) {
+                return likes; // Return all likes
+            }
+        },
+        tags: {
+            type: new GraphQLList(TagType),
+            resolve(parent, args) {
+                return tags; // Return all tags
+            }
+        }
+    }
+});
+
+
+// CREATE SCHEMA
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
